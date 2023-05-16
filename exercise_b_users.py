ed85f29ca9eb77ae61e5b691de93577bcacbe6b7
Randolph Lam <randolph.lam@codeclan.com>
2023-05-16 16:06:00 +0100

    second commit

--- exercise_b_users.py
+++ exercise_b_users.py
@@ -1,60 +1,35 @@
 users = {
-  "Jonathan": {
-    "twitter": "jonnyt",
-    "lottery_numbers": [6, 12, 49, 33, 45, 20],
-    "home_town": "Stirling",
-    "pets": [
-    {
-      "name": "fluffy",
-      "species": "cat"
+    "Jonathan": {
+        "twitter": "jonnyt",
+        "lottery_numbers": [6, 12, 49, 33, 45, 20],
+        "home_town": "Stirling",
+        "pets": [
+            {"name": "fluffy", "species": "cat"},
+            {"name": "fido", "species": "dog"},
+            {"name": "spike", "species": "dog"},
+        ],
     },
-    {
-      "name": "fido",
-      "species": "dog"
+    "Erik": {
+        "twitter": "eriksf",
+        "lottery_numbers": [18, 34, 8, 11, 24],
+        "home_town": "Linlithgow",
+        "pets": [
+            {"name": "nemo", "species": "fish"},
+            {"name": "kevin", "species": "fish"},
+            {"name": "spike", "species": "dog"},
+            {"name": "rupert", "species": "parrot"},
+        ],
     },
-    {
-      "name": "spike",
-      "species": "dog"
-    }
-  ]
-  },
-  "Erik": {
-    "twitter": "eriksf",
-    "lottery_numbers": [18, 34, 8, 11, 24],
-    "home_town": "Linlithgow",
-    "pets": [
-    {
-      "name": "nemo",
-      "species": "fish"
+    "Avril": {
+        "twitter": "bridgpally",
+        "lottery_numbers": [12, 14, 33, 38, 9, 25],
+        "home_town": "Dunbar",
+        "pets": [{"name": "monty", "species": "snake"}],
     },
-    {
-      "name": "kevin",
-      "species": "fish"
-    },
-    {
-      "name": "spike",
-      "species": "dog"
-    },
-    {
-      "name": "rupert",
-      "species": "parrot"
-    }
-  ]
-  },
-  "Avril": {
-    "twitter": "bridgpally",
-    "lottery_numbers": [12, 14, 33, 38, 9, 25],
-    "home_town": "Dunbar",
-    "pets": [
-      {
-        "name": "monty",
-        "species": "snake"
-      }
-    ]
-  }
 }
 
 # 1. Get Jonathan's Twitter handle (i.e. the string `"jonnyt"`)
+print(users["Jonathan"]["twitter"])
 # 2. Get Erik's hometown
 # 3. Get the list of Erik's lottery numbers
 # 4. Get the species of Avril's pet Monty
